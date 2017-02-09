b04a70212cf68c92578c21436835f7b4f22e91b3
Nikolaj Kæmpe - Laptop <NKN@airship.one>
2017-02-09 10:14:20 +0100

    Initial commit

---
+++ src/main/java/Controllers/UserController.java
@@ -0,0 +1,48 @@
+package Controllers;
+
+import ErrorHandling.ResponseError;
+import Models.User;
+import Services.UserService;
+import static JsonUtil.JsonUtil.*;
+import static spark.Spark.*;
+
+public class UserController {
+
+    public UserController(final UserService userService) {
+
+        userService.createUser("Mark","Mark@mark.kram");
+        userService.createUser("Thomas","thomas@mh.dk");
+
+        get("/users", (req, res) -> userService.getAllUsers(), json());
+
+        get("/users/:id", (req, res) -> {
+            String id = req.params(":id");
+            User user = userService.getUser(id);
+            if (user != null) {
+                return user;
+            }
+            res.status(400);
+            return new ResponseError("No user with id '%s' found", id);
+        }, json());
+
+        post("/users", (req, res) -> userService.createUser(
+                req.queryParams("name"),
+                req.queryParams("email")
+        ), json());
+
+        put("/users/:id", (req, res) -> userService.updateUser(
+                req.params(":id"),
+                req.queryParams("name"),
+                req.queryParams("email")
+        ), json());
+
+        after((req, res) -> {
+            res.type("application/json");
+        });
+
+        exception(IllegalArgumentException.class, (e, req, res) -> {
+            res.status(400);
+            res.body(toJson(new ResponseError(e)));
+        });
+    }
+}
---
+++ src/main/java/Models/User.java
@@ -0,0 +1,46 @@
+package Models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+
+@DatabaseTable(tableName = "AirshipOne")
+public class User
+{
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private String name;
+
+    @DatabaseField
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
